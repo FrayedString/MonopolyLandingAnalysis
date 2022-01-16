74f5903331d5c34060c43271fd1293cedda25f43
FrayedString <frayed@frayed.net>
2022-01-15 21:40:38 -0800

    Fix naming of chance deck in init_chance()

--- src/game_objects.rs
+++ src/game_objects.rs
@@ -171,17 +171,17 @@ fn init_community_chest() -> GameActionCardDeck {
 
 
 fn init_chance() -> GameActionCardDeck {
-    let mut comm_chance_cards: Vec<_> = Vec::<GameActionCard>::with_capacity(16);
+    let mut chance_cards: Vec<_> = Vec::<GameActionCard>::with_capacity(16);
 
 
     //CARDS FROM: https://www.monopolyland.com/list-monopoly-chance-community-chest-cards/
 
         //Initialize cards into the Dealt vec, then shuffle on use
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to Boardwalk"), move_action: |_| Some(39) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to Go (Collect $200)"), move_action: |_| Some(0) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to Illinois Avenue. If you pass Go, collect $200"), move_action: |_| Some(24) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to St. Charles Place. If you pass Go, collect $200"), move_action: |_| Some(11) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled"), move_action: |landed| {
+        chance_cards.push(GameActionCard { text: String::from("Advance to Boardwalk"), move_action: |_| Some(39) });
+        chance_cards.push(GameActionCard { text: String::from("Advance to Go (Collect $200)"), move_action: |_| Some(0) });
+        chance_cards.push(GameActionCard { text: String::from("Advance to Illinois Avenue. If you pass Go, collect $200"), move_action: |_| Some(24) });
+        chance_cards.push(GameActionCard { text: String::from("Advance to St. Charles Place. If you pass Go, collect $200"), move_action: |_| Some(11) });
+        chance_cards.push(GameActionCard { text: String::from("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled"), move_action: |landed| {
             match landed {
                 7 => Some(15),  //Pennsylvania Railroad
                 22 => Some(25),  //B&O Railroad
@@ -189,7 +189,7 @@ fn init_chance() -> GameActionCardDeck {
                 _ => None
             }
         }});
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled"), move_action: |landed| {
+        chance_cards.push(GameActionCard { text: String::from("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled"), move_action: |landed| {
             match landed {
                 7 => Some(15),  //Pennsylvania Railroad
                 22 => Some(25),  //B&O Railroad
@@ -197,24 +197,24 @@ fn init_chance() -> GameActionCardDeck {
                 _ => None
             }
         }});
-        comm_chance_cards.push(GameActionCard { text: String::from("Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown."), move_action: |landed| {
+        chance_cards.push(GameActionCard { text: String::from("Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown."), move_action: |landed| {
             match landed {
                 7 | 36 => Some(12),  //Electric Company
                 22 => Some(28),  //Water Works
                 _ => None
             }
         }});
-        comm_chance_cards.push(GameActionCard { text: String::from("Bank pays you dividend of $50"), move_action: |_| None });
-        comm_chance_cards.push(GameActionCard { text: String::from("Get Out of Jail Free"), move_action: |_| None });
-        comm_chance_cards.push(GameActionCard { text: String::from("Go Back 3 Spaces"), move_action: |landed| Some(landed - 3) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Go to Jail. Go directly to jail, do not pass Go, do not collect $200"), move_action: |_| Some(10) });
-        comm_chance_cards.push(GameActionCard { text: String::from("Make general repairs on all your property. For each house pay $25. For each hotel pay $100"), move_action: |_| None });
-        comm_chance_cards.push(GameActionCard { text: String::from("Speeding fine $15"), move_action: |_| None });
-        comm_chance_cards.push(GameActionCard { text: String::from("Take a trip to Reading Railroad. If you pass Go, collect $200"), move_action: |_| Some(5) });
-        comm_chance_cards.push(GameActionCard { text: String::from("You have been elected Chairman of the Board. Pay each player $50"), move_action: |_| None });
-        comm_chance_cards.push(GameActionCard { text: String::from("Your building loan matures. Collect $150"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Bank pays you dividend of $50"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Get Out of Jail Free"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Go Back 3 Spaces"), move_action: |landed| Some(landed - 3) });
+        chance_cards.push(GameActionCard { text: String::from("Go to Jail. Go directly to jail, do not pass Go, do not collect $200"), move_action: |_| Some(10) });
+        chance_cards.push(GameActionCard { text: String::from("Make general repairs on all your property. For each house pay $25. For each hotel pay $100"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Speeding fine $15"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Take a trip to Reading Railroad. If you pass Go, collect $200"), move_action: |_| Some(5) });
+        chance_cards.push(GameActionCard { text: String::from("You have been elected Chairman of the Board. Pay each player $50"), move_action: |_| None });
+        chance_cards.push(GameActionCard { text: String::from("Your building loan matures. Collect $150"), move_action: |_| None });
 
-        GameActionCardDeck::new(comm_chance_cards)
+        GameActionCardDeck::new(chance_cards)
 }
 
 
